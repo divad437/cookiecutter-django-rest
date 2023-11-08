1cdae10af1adfde80eebeb269fca0c9d3c675b13
divad <132532789+divad437@users.noreply.github.com>
2023-11-08 02:19:24 +0100

    modification of admin.py file

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/admin.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/admin.py
@@ -2,11 +2,43 @@ from django.conf import settings
 from django.contrib import admin
 from django.contrib.auth import admin as auth_admin
 from django.contrib.auth import get_user_model, decorators
+from django.contrib.auth import forms as admin_forms
+{%- if cookiecutter.username_type == "email" %}
+from django.forms import EmailField
+{%- endif %}
 from django.utils.translation import gettext_lazy as _
 
-from {{ cookiecutter.project_slug }}.users.forms import UserAdminChangeForm, UserAdminCreationForm
 
 User = get_user_model()
+
+
+class UserAdminChangeForm(admin_forms.UserChangeForm):
+    class Meta(admin_forms.UserChangeForm.Meta):
+        model = User
+        {%- if cookiecutter.username_type == "email" %}
+        field_classes = {"email": EmailField}
+        {%- endif %}
+
+
+class UserAdminCreationForm(admin_forms.UserCreationForm):
+    """
+    Form for User Creation in the Admin Area.
+    To change user signup, see UserSignupForm and UserSocialSignupForm.
+    """
+
+    class Meta(admin_forms.UserCreationForm.Meta):
+        model = User
+        {%- if cookiecutter.username_type == "email" %}
+        fields = ("email",)
+        field_classes = {"email": EmailField}
+        error_messages = {
+            "email": {"unique": _("This email has already been taken.")},
+        }
+        {%- else %}
+        error_messages = {
+            "username": {"unique": _("This username has already been taken.")},
+        }
+        {%- endif %}
 
 
 @admin.register(User)
