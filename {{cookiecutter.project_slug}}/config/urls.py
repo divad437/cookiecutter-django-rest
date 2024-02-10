e12f43a44f027aa9e273dd2734c7844f5440daf7
divad <132532789+divad437@users.noreply.github.com>
2024-02-10 06:00:38 +0100

    adding some conditions to urls config

--- {{cookiecutter.project_slug}}/config/urls.py
+++ {{cookiecutter.project_slug}}/config/urls.py
@@ -8,7 +8,7 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 
 urlpatterns = [
-{% if cookiecutter.use_djoser == 'n' %}
+{% if cookiecutter.use_djoser == 'n' and cookiecutter.use_simplejwt == 'n' %}
     path("auth-token/", obtain_auth_token),
 {% endif %}
     path("api/schema/", SpectacularAPIView.as_view(), name="api-schema"),
@@ -21,7 +21,7 @@ urlpatterns = [
     path('api/auth/', include('djoser.urls')),
     path('api/auth/', include('djoser.urls.jwt')),
 {%- else %}
-{%- if cookiecutter.use_simplejwt == 'y' %}
+{%- if cookiecutter.use_simplejwt == 'y' and cookiecutter.use_djoser == 'y' %}
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 {%- endif %}
